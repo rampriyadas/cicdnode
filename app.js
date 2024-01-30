4276d754e090e214a53154bc15d312bfe8091c37
dev_ram <ram@gisfy.co.in>
2024-01-30 19:32:02 +0530

    to faiz bro

--- app.js
+++ app.js
@@ -5,6 +5,6 @@ app.use(express.json())
 
 app.get('/',(req,res)=>{
     res.status(200)
-    res.json({"message":"Okay running"})
+    res.json({"message":"Okay running check faiz"})
 })
 app.listen(8000,console.log(`listening to port : 8000`))
