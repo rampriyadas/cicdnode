ee3df08ada0c3f66fc562f208e79d2d5f2c68958
rampriyadas <rampriyadas@github.com>
2024-02-16 22:48:51 +0530

    added android api

--- endpoints/paths.js
+++ endpoints/paths.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const controller = require('../handdlers/shapefile')
 const khsaraDetails = require('../handdlers/khsara')
+const getdtls = require('../handdlers/android')
 const router = express.Router()
 
 router.route('/shapefile').post(controller.postData)
 router.route('/khasra').post(khsaraDetails.getData)
 
+router.route('/getmethod').get(getdtls.getData)
+router.route('/postmethod').post(getdtls.postData)
 
 module.exports = router
