ce50d1d8802a9f5d7c5684100babb91a1b79368c
rampriyadas <rampriyadas@github.com>
2024-02-16 23:35:59 +0530

    okay

--- handdlers/android.js
+++ handdlers/android.js
@@ -5,7 +5,7 @@ const getData = async (req,res)=>{
 }
 const postData = async (req,res)=>{
    
-  if(req.body.name !=''){
+  if(req.body.name){
     res.status(200).json({"message":`Hey, how are you ${req.body.name} :)`})
   }
   else{
