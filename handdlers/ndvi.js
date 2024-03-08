653c9d02ffe51cfaa77cf7e9125169063fe66c0d
dev_ram <ram@gisfy.co.in>
2024-03-08 18:36:34 +0530

    added port and plocy

--- handdlers/ndvi.js
+++ handdlers/ndvi.js
@@ -2,6 +2,47 @@ const khasra = require('../schema/khasra')
 const axios = require("axios")
 const gettoken = require('../config/gettoken')
 
+evalscript = `
+//VERSION=3
+function setup() {
+  return {
+    input: [{
+      bands: [
+        "B04",
+        "B08",
+        "SCL",
+        "dataMask"
+      ]
+    }],
+    mosaicking: "ORBIT",
+    output: [
+      {
+        id: "data",
+        bands: ["monthly_max_ndvi"]
+      },
+      {
+        id: "dataMask",
+        bands: 1
+      }]
+  }
+}
+
+function evaluatePixel(samples) {
+    var max = 0;
+    var hasData = 0;
+    for (var i=0;i<samples.length;i++) {
+      if (samples[i].dataMask == 1 && samples[i].SCL != 6 && samples[i].B04+samples[i].B08 != 0 ){
+        hasData = 1
+        var ndvi = (samples[i].B08 - samples[i].B04)/(samples[i].B08 + samples[i].B04);
+        max = ndvi > max ? ndvi:max;
+      }
+    }
+
+    return {
+        data: [max],
+        dataMask: [hasData]
+    }
+}`
 const getX = async (req,res)=>{
  if(req.body.khasra!=''){
     let details = await khasra.findOne({"khasra":req.body.khasra,"village":req.body.village}).select({ goemetry: 1, _id: 0 });
